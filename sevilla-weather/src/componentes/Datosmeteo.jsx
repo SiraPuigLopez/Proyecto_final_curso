5a2c1b6cab48690809fbe114521ed911ad0a0c03
Sirap <sirapuiglopez@gmail.com>
2022-07-28 19:41:50 +0200

    icono_y_.env

--- sevilla-weather/src/componentes/Datosmeteo.jsx
+++ sevilla-weather/src/componentes/Datosmeteo.jsx
@@ -3,8 +3,6 @@ import { WiDaySunny, WiCloudy, WiRain } from "react-icons/wi";
 import "../estilos/Datosmeteo.css";
 
 const Datosmeteo = (props) => {
-  console.log(props);
-
   return (
     <div>
       <ul>
@@ -12,8 +10,10 @@ const Datosmeteo = (props) => {
         <li>Viento {props.ciudad.wind.speed}</li>
         <li>Temperatura {props.ciudad.main.temp}</li>
         <li>
-          <WiDaySunny className="simbolo"></WiDaySunny>
-          <img src="" alt=""></img>
+          <img
+            src={`http://openweathermap.org/img/wn/${props.ciudad.weather[0].icon}@2x.png`}
+            alt="icono"
+          />
         </li>
         <li>Max {props.ciudad.main.temp_max}</li>
         <li>Min {props.ciudad.main.temp_min}</li>
